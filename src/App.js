58dcf2a0c0f131d94508343b0cb96403261332ef
EloiGry <eloi.grychta@gmail.com>
2021-11-23 15:28:23 +0100

    card.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import styled from 'styled-components'
+import Card from './components/Card';
 
 
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Card />
       <Routes>
-        <Route exact path="/" element={<Home/>}> </Route>
+        {/* <Route exact path="/" element={<Home/>}> </Route>
         <Route path="/city" element={<City/>}> </Route>
         <Route path="/hotels" element={<Hotels/>}> </Route>
         <Route path="/hotel" element={<Hotel/>}> </Route>
         <Route path="/room" element={<Room/>}> </Route>
         <Route path="/favorite" element={<Favorite/>}> </Route>
-        <Route path="*" element={<NotFound/>}> </Route>
+        <Route path="*" element={<NotFound/>}> </Route> */}
       </Routes>
     </BrowserRouter>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+
+const IMG = styled.img`
+  height:200px;
+  width: 100%;
+  border-radius: 15px 15px 0px 0px
+`
+const Container = styled.div`
+  border : 1px solid #C8C8C8;
+  margin : 30px 20px;
+  border-radius: 15px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;;
+`
+const Title = styled.h3`
+  text-align : center;`
+
 
 const Card = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    const [cards, setCard] = useState([])
+
+    useEffect(() => { 
+        fetch("https://trippy-konexio.herokuapp.com/api/home")
+        .then(response => response.json())
+        .then(result => {
+          setCard(result.cities)
+        })}, []);
+
+    if (cards == null ) {
+        return null
+    } else {
+        return (
+            <div>
+                {cards.map(card => 
+                    <Container>
+                        <IMG src={`https://trippy-konexio.herokuapp.com${card.source}`} alt={card.slug} />
+                        <Title> {card.name} </Title>
+                    </Container>
+                )}
+            </div>
+        );
+    }
+    
+}
 
 export default Card;
