83dbf115389492f81b7f240284d24cfa0e1cd613
EloiGry <eloi.grychta@gmail.com>
2021-11-23 14:00:09 +0100

    branch test + app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>}> </Route>
+        <Route path="/city" element={<City/>}> </Route>
+        <Route path="/hotels" element={<Hotels/>}> </Route>
+        <Route path="/hotel" element={<Hotel/>}> </Route>
+        <Route path="/room" element={<Room/>}> </Route>
+        <Route path="/favorite" element={<Favorite/>}> </Route>
+        <Route path="*" element={<NotFound/>}> </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
