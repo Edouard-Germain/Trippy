24d07a3ab2cf97cfd3301f3a7ec1b8ae179e0d6f
EloiGry <eloi.grychta@gmail.com>
2021-11-24 15:06:18 +0100

    city.js

--- src/Pages/City.js
+++ src/Pages/City.js
@@ -8,7 +8,21 @@ import styled from 'styled-components';
 
 const MapContainer = styled.div`
 height: 100vh;
-width: 100%;`
+width: 70%;`
+
+const ListContainer = styled.div`
+height: 100vh
+width: 30%;
+overflow: scroll;
+`
+const List = styled.div`
+border: 1px solid lightgray;
+margin : 10px 5px;
+border-radius: 10px
+`
+const Container = styled.div`
+display : flex
+`
 
 const City = () => {
     const {city} = useParams()
@@ -27,21 +41,23 @@ const City = () => {
         console.log(hotels)
         return (
         
-            <>
-                <div> 
+            <Container>
+                <ListContainer> 
                 {hotels.results.map(hotel =>
-                   <>
+                   <List>
                     <p> {hotel.name} </p>
                     <p> {hotel.phone} </p>
                     <p> {hotel.stars} </p>
-                   </> 
+                   </List> 
                 )}
-                </div>
+                </ListContainer>
                 <MapContainer>
                     <GoogleMapReact
                         bootstrapURLKeys={{ key: "" }}
-                        defaultCenter={hotels.center}
-                        defaultZoom={14}
+                        defaultCenter={{
+                            lat : hotels.center.lat,
+                            lng : hotels.center.lon}}
+                        defaultZoom={12}
                     >
                         {hotels.results.map(hotel =>
                         <Marker
@@ -53,7 +69,7 @@ const City = () => {
                     </GoogleMapReact>
                 </MapContainer>
                 
-            </>
+            </Container>
                
         );
     }
--- src/components/Marker.js
+++ src/components/Marker.js
@@ -6,14 +6,25 @@ const MarkerContainer = styled.div`
   height: 40px;
   position: relative;
 `
+const Price = styled.div`
+    border : 1px solid #dee2e6;
+    background-color : #dee2e6;
+    text-align: center;
+    border-radius: 20px;
+    font-weight: 600;
+    position : absolute;
+    left : -21px;
+    padding: 5px
+`
 
 
-const Marker = () => {
+const Marker = (props) => {
 
     
   return (
       <>
         <MarkerContainer>
+        <Price> {props.price}€ </Price>
         <FaMapMarkerAlt style={{ width: '30px', height: '30px', color: "red", position: "absolute", bottom: '100%', left: '-20px' }} />
         </MarkerContainer>
      </>
